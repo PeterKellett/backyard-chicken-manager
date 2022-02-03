be7a4e3d7f0fcbb05bc41b128a9c04ec6d388156
PeterKellett <peterwkellett@gmail.com>
2022-02-03 18:16:00 +0000

    Set up Profiles app and models

---
+++ profiles/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from customAuth.models import CustomUser
+
+
+# Create your models here.
+class Languages(models.Model):
+    language = models.CharField(max_length=20,
+                                null=False,
+                                blank=False)
+
+
+class Currency(models.Model):
+    currency = models.CharField(max_length=20,
+                                null=False,
+                                blank=False)
+
+
+class FarmType(models.Model):
+    type = models.CharField(max_length=20,
+                            null=False,
+                            blank=False)
+
+    def __str__(self):
+        return self.type
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20,
+                                  null=False,
+                                  blank=False)
+    last_name = models.CharField(max_length=20,
+                                 null=False,
+                                 blank=False)
+    city_country = models.CharField(max_length=20,
+                                    null=False,
+                                    blank=False)
+
+    def __str__(self):
+        return self.user
+
+
+class FarmProfile(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    farm_name = models.CharField(max_length=20,
+                                 null=False,
+                                 blank=False)
+    farm_type = models.ForeignKey(FarmType, on_delete=models.PROTECT)
+    farm_sales_roadside = models.BooleanField()
+    farm_sales_markets = models.BooleanField()
+    farm_sales_deliveries = models.BooleanField()
+    farm_sales_collections = models.BooleanField()
+    eggs_in_stock = models.IntegerField(default=0,
+                                        null=False,
+                                        blank=False)
+    current_balance = models.DecimalField(max_digits=10,
+                                          decimal_places=2,
+                                          null=False,
+                                          default=0)
+
+    def __str__(self):
+        return self.user
