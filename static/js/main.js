8e1ce7883f12b18cc282826e9f48ae8d8299cd1b
CathalDolan <cathal@gizagig.com>
2022-02-10 23:03:29 +0000

    Updated onboarding personal to remove Farm page and put in JS for hcc qty validation

--- static/js/main.js
+++ static/js/main.js
@@ -12,3 +12,26 @@ $(document).ready(function(){
       x.type = "password";
     }
   }
+
+  // Calculation to sum total number of birds in HCC section
+  function hccTotal(){
+      var totalHens = document.querySelector('input[name=hens-qty]').value;
+      var totalChicks = document.querySelector('input[name=chicks-qty]').value;
+      var totalCocks = document.querySelector('input[name=cocks-qty]').value;
+      var total = Number(totalHens) + Number(totalChicks) + Number(totalCocks);
+      document.getElementById("total-birds").textContent = total;
+  }
+
+  // HCC Section: Ensures at least one input is selected when there are multiple options
+  function validateQtys(){
+      if( document.getElementById('total-birds').value == null){
+        document.getElementById("bird-qty-warning").textContent = "Please provide at least one quantity for Hens, Chicks or Cocks.";
+        return false;
+      }
+  }
+
+  function validateMethods(){
+      if( document.querySelector('input[class=sales-methods]').checked = false)(
+        console.log("False")
+      )
+  }
