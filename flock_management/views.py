4ca7a6e8a97329e1aa533d8bab04d0f98f004c4e
CathalDolan <cathal@gizagig.com>
2022-02-15 16:33:15 +0000

    Started flack management html and updated margin-bottom 10px in css by adding row class to affected elements

--- flock_management/views.py
+++ flock_management/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from customAuth.models import CustomUser
 
-# Create your views here.
+
+@login_required
+def flocks(request):
+    """view to current flock"""
+    template = 'flock_management/flocks.html'
+    context = {}
+    return render(request, template, context)
+
+@login_required
+def add_flock(request):
+    """view to add flock"""
+    template = 'flock_management/add_flock.html'
+    context = {}
+    return render(request, template, context)
