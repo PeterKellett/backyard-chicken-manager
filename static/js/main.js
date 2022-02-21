4db6aacf639b1986b08e481857fa7b91ff1f7412
CathalDolan <cathal@gizagig.com>
2022-02-21 21:28:33 +0000

    Fixed JS function to autosuggest city and country

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     console.log("Document ready!");
+
     // PK function
     var flock_form_iteration = 0;
-
     function addFlockForm() {
         console.log("flock_form_iteration " + flock_form_iteration);
         $("#form-data").append(
@@ -127,107 +127,107 @@ $(document).ready(function () {
             salesUnits = false
         }
 
-    if (salesMethods == true && salesUnits == true){
-        window.location.href = "{% url 'onboard_stock' %}";
-        } else {
-            document.getElementById('sales-unit-method-warning').textContent =
-                "Please select at least one option each from Sales Methods and Sales Units";
+        if (salesMethods == true && salesUnits == true){
+            window.location.href = "{% url 'onboard_stock' %}";
+            } else {
+                document.getElementById('sales-unit-method-warning').textContent =
+                    "Please select at least one option each from Sales Methods and Sales Units";
+            }
         }
-    }
 
     // Adds another set of fields fields when the "Add Another" button is clicked
-    /*function addAnotherFlock() {
+    function addAnotherFlock() {
         var flockCodeBlock = '<div>' +
-                                '<hr class="hr-small">' +
-                            '</div>' +
-                                '<div class="container form-data">' +
-                        '<div class="row mb-0" id="hens-row">' +
-                            '<div class="col-1 col-css">' +
-                                '<img src="{{ MEDIA_URL }}hen.png" alt="Chicken" class="col-css-element">' +
-                            '</div>' +
-                            '<div class="col-4 col-css">' +
-                                '<h4 class="row-name col-css-element">Hens</h4>' +
-                            '</div>' +
-                            '<div class="col-3 col-css">' +
-                                '<p class="checkbox-label hcc-checkbox-label col-css-element">All Hens</p>' +
-                            '</div>' +
-                            '<div class="col-1 col-css">' +
-                                '<input type="checkbox" name="all_hens_check" id="all_hens_check" class="col-css-element hcc-checkbox">' +
-                            '</div>' +
-                            '<div class="col-3 col-css col-css-right">' +
-                                '{% if "hens_qty" in onboard_profile_data %}' +
-                                '<input type="number" name="hens-qty" value="{{ onboard_profile_data.hens_qty }}" placeholder="Qty" id="hens-qty" ' +
-                                    'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                                '{% else %}' +
-                                '<input type="number" name="hens-qty" placeholder="Qty" id="hens_qty" ' +
-                                    'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                                '{% endif %}' +
-                            '</div>' +
-                        '</div>' +
-                        '<div>' +
                             '<hr class="hr-small">' +
                         '</div>' +
-
-                        '<div class="row mb-0" id="chicks-row">' +
-                            '<div class="col-1 col-css">' +
-                                '<img src="{{ MEDIA_URL }}hen.png" alt="Chicks" class="col-css-element">' +
-                            '</div>' +
-                            '<div class="col-4 col-css">' +
-                                '<h4 class="row-name col-css-element">Chicks</h4>' +
-                            '</div>' +
-                            '<div class="col-3 col-css">' +
-                                '<p class="checkbox-label hcc-checkbox-label col-css-element">All Chicks</p>' +
-                            '</div>' +
-                            '<div class="col-1 col-css">' +
-                                '<input type="checkbox" name="all_chicks_check" id="all_chicks_check" class="col-css-element hcc-checkbox">' +
-                            '</div>' +
-
-                            '<div class="col-3 col-css col-css-right">' +
-                                '{% if "chicks_qty" in onboard_profile_data %}' +
-                                '<input type="number" name="chicks-qty" value="{{ onboard_profile_data.chicks_qty }}" placeholder="Qty" id="chicks-qty" ' +
-                                'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                                '{% else %}' +
-                                '<input type="number" name="chicks-qty" placeholder="Qty" id="chicks_qty" ' +
-                                    'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                                '{% endif %}' +
-
-                            '</div>' +
+                            '<div class="container form-data">' +
+                    '<div class="row mb-0" id="hens-row">' +
+                        '<div class="col-1 col-css">' +
+                            '<img src="{{ MEDIA_URL }}hen.png" alt="Chicken" class="col-css-element">' +
                         '</div>' +
-                        '<div>' +
-                            '<hr class="hr-small">' +
+                        '<div class="col-4 col-css">' +
+                            '<h4 class="row-name col-css-element">Hens</h4>' +
                         '</div>' +
-
-                        '<div class="row mb-0" id="cocks-row">' +
-                            '<div class="col-1 col-css">' +
-                                '<img src="{{ MEDIA_URL }}hen.png" alt="Cocks" class="col-css-element">' +
-                            '</div>' +
-                            '<div class="col-4 col-css">' +
-                                '<h4 class="row-name col-css-element">Cocks</h4>' +
-                            '</div>' +
-                            '<div class="col-3 col-css">' +
-                                '<p class="checkbox-label hcc-checkbox-label col-css-element">All Cocks</p>' +
-                            '</div>' +
-                            '<div class="col-1 col-css">' +
-                                '<input type="checkbox" name="all_cocks_check" id="all_cocks_check" class="col-css-element hcc-checkbox">' +
-                            '</div>' +
-                            '<div class="col-3 col-css col-css-right">' +
-                                '{% if "cocks_qty" in onboard_profile_data %}' +
-                                '<input type="number" name="cocks-qty" value="{{ onboard_profile_data.cocks_qty }}" placeholder="Qty" id="cocks-qty" ' +
+                        '<div class="col-3 col-css">' +
+                            '<p class="checkbox-label hcc-checkbox-label col-css-element">All Hens</p>' +
+                        '</div>' +
+                        '<div class="col-1 col-css">' +
+                            '<input type="checkbox" name="all_hens_check" id="all_hens_check" class="col-css-element hcc-checkbox">' +
+                        '</div>' +
+                        '<div class="col-3 col-css col-css-right">' +
+                            '{% if "hens_qty" in onboard_profile_data %}' +
+                            '<input type="number" name="hens-qty" value="{{ onboard_profile_data.hens_qty }}" placeholder="Qty" id="hens-qty" ' +
                                 'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                                '{% else %}' +
-                                '<input type="number" name="cocks-qty" placeholder="Qty" id="cocks-qty" ' +
-                                    'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                                '{% endif %}' +
-                            '</div>' +
+                            '{% else %}' +
+                            '<input type="number" name="hens-qty" placeholder="Qty" id="hens_qty" ' +
+                                'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
+                            '{% endif %}' +
                         '</div>' +
                     '</div>' +
-                '</div>';
-    document.querySelector('div[name=flocks-container]').outerHTML = flockCodeBlock;
-}
+                    '<div>' +
+                        '<hr class="hr-small">' +
+                    '</div>' +
+
+                    '<div class="row mb-0" id="chicks-row">' +
+                        '<div class="col-1 col-css">' +
+                            '<img src="{{ MEDIA_URL }}hen.png" alt="Chicks" class="col-css-element">' +
+                        '</div>' +
+                        '<div class="col-4 col-css">' +
+                            '<h4 class="row-name col-css-element">Chicks</h4>' +
+                        '</div>' +
+                        '<div class="col-3 col-css">' +
+                            '<p class="checkbox-label hcc-checkbox-label col-css-element">All Chicks</p>' +
+                        '</div>' +
+                        '<div class="col-1 col-css">' +
+                            '<input type="checkbox" name="all_chicks_check" id="all_chicks_check" class="col-css-element hcc-checkbox">' +
+                        '</div>' +
+
+                        '<div class="col-3 col-css col-css-right">' +
+                            '{% if "chicks_qty" in onboard_profile_data %}' +
+                            '<input type="number" name="chicks-qty" value="{{ onboard_profile_data.chicks_qty }}" placeholder="Qty" id="chicks-qty" ' +
+                            'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
+                            '{% else %}' +
+                            '<input type="number" name="chicks-qty" placeholder="Qty" id="chicks_qty" ' +
+                                'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
+                            '{% endif %}' +
+
+                        '</div>' +
+                    '</div>' +
+                    '<div>' +
+                        '<hr class="hr-small">' +
+                    '</div>' +
+
+                    '<div class="row mb-0" id="cocks-row">' +
+                        '<div class="col-1 col-css">' +
+                            '<img src="{{ MEDIA_URL }}hen.png" alt="Cocks" class="col-css-element">' +
+                        '</div>' +
+                        '<div class="col-4 col-css">' +
+                            '<h4 class="row-name col-css-element">Cocks</h4>' +
+                        '</div>' +
+                        '<div class="col-3 col-css">' +
+                            '<p class="checkbox-label hcc-checkbox-label col-css-element">All Cocks</p>' +
+                        '</div>' +
+                        '<div class="col-1 col-css">' +
+                            '<input type="checkbox" name="all_cocks_check" id="all_cocks_check" class="col-css-element hcc-checkbox">' +
+                        '</div>' +
+                        '<div class="col-3 col-css col-css-right">' +
+                            '{% if "cocks_qty" in onboard_profile_data %}' +
+                            '<input type="number" name="cocks-qty" value="{{ onboard_profile_data.cocks_qty }}" placeholder="Qty" id="cocks-qty" ' +
+                            'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
+                            '{% else %}' +
+                            '<input type="number" name="cocks-qty" placeholder="Qty" id="cocks-qty" ' +
+                                'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
+                            '{% endif %}' +
+                        '</div>' +
+                    '</div>' +
+                '</div>' +
+            '</div>';
+        document.querySelector('div[name=flocks-container]').outerHTML = flockCodeBlock;
+        }
 
 // Adds another set of feeds fields when the "Add Another" button is clicked
-function addAnotherFeed() {
-    var feedsCodeBlock = `<div class="row">
+    function addAnotherFeed() {
+        var feedsCodeBlock = `<div class="row">
                             <hr class="hr-small">
                         </div>
                         <div class="row">
@@ -247,11 +247,11 @@ function addAnotherFeed() {
                             </div>
                         </div>
                         <div class="container" name="feeds-container"></div>`
-    document.querySelector('div[name=feeds-container]').outerHTML = feedsCodeBlock;
-}
+        document.querySelector('div[name=feeds-container]').outerHTML = feedsCodeBlock;
+        }
 
-// Adds another set of supplements fields when the "Add Another" button is clicked
-function addAnotherSupplement() {
+    // Adds another set of supplements fields when the "Add Another" button is clicked
+    function addAnotherSupplement() {
     var supplementsCodeBlock = '<div class="row">' +
                                     '<hr class="hr-small">' +
                                 '</div>' +
@@ -271,10 +271,38 @@ function addAnotherSupplement() {
                                     '</div>' +
                                 '</div>' +
                                 '<div class="container" name="supplements-container"></div>';
-    document.querySelector('div[name=supplements-container]').outerHTML = supplementsCodeBlock;
-}
+        document.querySelector('div[name=supplements-container]').outerHTML = supplementsCodeBlock;
+        }
 
-// Functions to autosuggest cities
+    
+
+    // Function to make textarea height to expand based on amopunt of text added by the user
+    // Taken from https://www.techiedelight.com/automatically-resize-textarea-height-javascript/
+    $(document).ready(function() {
+        $('textarea').on('keyup keypress', function() {
+            $(this).height(0);
+            $(this).height(this.scrollHeight);
+        });
+    });
+
+    // Function to display additional fields or content when a checkbpx is checked
+    // Taken from http://jsfiddle.net/TrueBlueAussie/DLQY9/1/
+    $(function () {
+        $('input[name="disinfectant_name"]').hide();
+
+        //show it when the checkbox is clicked
+        $('input[name="disinfected-check"]').on('click', function () {
+            if ($(this).prop('checked')) {
+                $('input[name="disinfectant_name"]').fadeIn();
+            } else {
+                $('input[name="disinfectant_name"]').hide();
+            }
+        });
+    });
+
+})
+
+// Functions to autosuggest cities. Must exist outside of (document).ready function.
 // Taken from https://www.youtube.com/watch?v=c3MjU9E9buQ&t=165s
 let autocomplete;
 function initAutocomplete() {
@@ -286,7 +314,7 @@ function initAutocomplete() {
             fields: ['address_components','geometry'] //https://developers.google.com/maps/documentation/javascript/place-data-fields?hl=en
         });
     autocomplete.addListener('place_changed', onPlaceChanged);
-}
+    }
 
 function onPlaceChanged() {
     var place = autocomplete.getPlace();
@@ -298,27 +326,3 @@ function onPlaceChanged() {
         document.getElementById('city_country').innerHTML = place.name;
     }
 }
-
-// Function to make textarea height to expand based on amopunt of text added by the user
-// Taken from https://www.techiedelight.com/automatically-resize-textarea-height-javascript/
-$(document).ready(function() {
-    $('textarea').on('keyup keypress', function() {
-        $(this).height(0);
-        $(this).height(this.scrollHeight);
-    });
-});
-
-// Function to display additional fields or content when a checkbpx os checked
-// Taken from http://jsfiddle.net/TrueBlueAussie/DLQY9/1/
-$(function () {
-    $('input[name="disinfectant_name"]').hide();
-
-    //show it when the checkbox is clicked
-    $('input[name="disinfected-check"]').on('click', function () {
-        if ($(this).prop('checked')) {
-            $('input[name="disinfectant_name"]').fadeIn();
-        } else {
-            $('input[name="disinfectant_name"]').hide();
-        }
-    });
-});
