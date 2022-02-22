e2390bb8fb2ed4aaa63258fa96b60dac76eab65f
CathalDolan <cathal@gizagig.com>
2022-02-22 14:36:42 +0000

    Onboarding - Produce: Fixed JS function to allow for validation of checkboxes before User is allowed move to next page

--- static/js/main.js
+++ static/js/main.js
@@ -83,158 +83,91 @@ $(document).ready(function () {
         }
     }
 
-    // Calculation to sum total number of birds in HCC section
-    document.getElementById("hens-qty").addEventListener("onchange", hccTotal);
+    // HCC: Calculation to sum total number of birds
+    document.getElementById("cocks-qty").addEventListener("onchange", hccTotal);
     function hccTotal() {
         console.log("hccTotal function")
-        // var totalHens = document.querySelector('input[name=hens_qty]').value;
-        // console.log("totalHens: " + totalHens)
-        // var totalChicks = document.querySelector('input[name=chicks_qty]').value;
-        // var totalCocks = document.querySelector('input[name=cocks_qty]').value;
-        // var total = Number(totalHens) + Number(totalChicks) + Number(totalCocks);
-        // document.getElementById("total-birds").textContent = total;
+        var totalHens = document.querySelector('input[name=hens_qty]').value;
+        console.log("totalHens: " + totalHens);
+        var totalChicks = document.querySelector('input[name=chicks_qty]').value;
+        var totalCocks = document.querySelector('input[name=cocks_qty]').value;
+        var total = Number(totalHens) + Number(totalChicks) + Number(totalCocks);
+        console.log("totalBirds: " + total);
+        document.getElementById("total-birds").textContent = total;
     }
-
+    
     // HCC Section: Ensures at least one quantity is indicated
     function validateQtys() {
         if (document.getElementById('total-birds').value == null) {
             document.getElementById("bird-qty-warning").textContent =
                 "Please provide at least one quantity for Hens, Chicks or Cocks.";
             return false;
+            console.log("validateQtys: false");
+        } else {
+            console.log("validateQtys: true");
+        }
+    }
+    
+// !!!!!!! (document).ready function end
+})
+
+
+// Sales Methods & Units Function to ensure at least one checkbox
+// is checked in each section before next page can be loaded
+function injectHref() {
+ console.log("Inject Href");
+    var salesMethods = null;
+    var salesUnits = null;
+    if (document.getElementById('roadside-check').checked == true ||
+        document.getElementById('markets-check').checked == true ||
+        document.getElementById('deliveries-check').checked == true ||
+        document.getElementById('collections-check').checked == true) {
+        salesMethods = true;
+    } else {
+        salesMethods = false
+    }
+
+    if (document.getElementById('single-eggs-check').checked == true ||
+        document.getElementById('half-dozen-carton-check').checked == true ||
+        document.getElementById('dozen-carton-check').checked == true ||
+        document.getElementById('trays-check').checked == true) {
+        salesUnits = true;
+    } else {
+        salesUnits = false
+    }
+
+    if (salesMethods == true && salesUnits == true){
+        document.getElementById("produce-next-button").href = "{% url 'onboard_stock' %}";
+        document.getElementById("submit-form").submit();
+        console.log("if true" )
+    } else {
+        document.getElementById('sales-unit-method-warning').textContent =
+            "Please select at least one option each from Sales Methods and Sales Units";
+            document.getElementById("submit-form").addEventListener('submit', (event) => {
+                // stop form submission
+                event.preventDefault();
+            });
+            console.log("if else" )
         }
     }
 
-    // Sales Methods & Units Function to ensure at least one checkbox is checked
-    // in each section before next page can be loaded
-    function injectHref() {
-        var salesMethods = null;
-        var salesUnits = null;
-        if (document.getElementById('roadside-check').checked == true ||
-            document.getElementById('markets-check').checked == true ||
-            document.getElementById('deliveries-check').checked == true ||
-            document.getElementById('collections-check').checked == true) {
-            salesMethods = true;
-        } else {
-            salesMethods = false
-        }
 
-        if (document.getElementById('single-eggs-check').checked == true ||
-            document.getElementById('half-dozen-carton-check').checked == true ||
-            document.getElementById('dozen-carton-check').checked == true ||
-            document.getElementById('trays-check').checked == true) {
-            salesUnits = true;
-        } else {
-            salesUnits = false
-        }
+// FLOCKS: Adds another set of fields fields when the "Add Another" button is clicked
+function addAnotherFlock() {
+    var flockCodeBlock = `{% include 'html_includes/hens_chicks_cocks.html' %}`;
+    document.querySelector('div[name=flocks-container]').outerHTML = flockCodeBlock;
+    }
 
-        if (salesMethods == true && salesUnits == true){
-            window.location.href = "{% url 'onboard_stock' %}";
-            } else {
-                document.getElementById('sales-unit-method-warning').textContent =
-                    "Please select at least one option each from Sales Methods and Sales Units";
-            }
-        }
-
-    // Adds another set of fields fields when the "Add Another" button is clicked
-    function addAnotherFlock() {
-        var flockCodeBlock = '<div>' +
-                            '<hr class="hr-small">' +
-                        '</div>' +
-                            '<div class="container form-data">' +
-                    '<div class="row mb-0" id="hens-row">' +
-                        '<div class="col-1 col-css">' +
-                            '<img src="{{ MEDIA_URL }}hen.png" alt="Chicken" class="col-css-element">' +
-                        '</div>' +
-                        '<div class="col-4 col-css">' +
-                            '<h4 class="row-name col-css-element">Hens</h4>' +
-                        '</div>' +
-                        '<div class="col-3 col-css">' +
-                            '<p class="checkbox-label hcc-checkbox-label col-css-element">All Hens</p>' +
-                        '</div>' +
-                        '<div class="col-1 col-css">' +
-                            '<input type="checkbox" name="all_hens_check" id="all_hens_check" class="col-css-element hcc-checkbox">' +
-                        '</div>' +
-                        '<div class="col-3 col-css col-css-right">' +
-                            '{% if "hens_qty" in onboard_profile_data %}' +
-                            '<input type="number" name="hens-qty" value="{{ onboard_profile_data.hens_qty }}" placeholder="Qty" id="hens-qty" ' +
-                                'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                            '{% else %}' +
-                            '<input type="number" name="hens-qty" placeholder="Qty" id="hens_qty" ' +
-                                'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                            '{% endif %}' +
-                        '</div>' +
-                    '</div>' +
-                    '<div>' +
-                        '<hr class="hr-small">' +
-                    '</div>' +
-
-                    '<div class="row mb-0" id="chicks-row">' +
-                        '<div class="col-1 col-css">' +
-                            '<img src="{{ MEDIA_URL }}hen.png" alt="Chicks" class="col-css-element">' +
-                        '</div>' +
-                        '<div class="col-4 col-css">' +
-                            '<h4 class="row-name col-css-element">Chicks</h4>' +
-                        '</div>' +
-                        '<div class="col-3 col-css">' +
-                            '<p class="checkbox-label hcc-checkbox-label col-css-element">All Chicks</p>' +
-                        '</div>' +
-                        '<div class="col-1 col-css">' +
-                            '<input type="checkbox" name="all_chicks_check" id="all_chicks_check" class="col-css-element hcc-checkbox">' +
-                        '</div>' +
-
-                        '<div class="col-3 col-css col-css-right">' +
-                            '{% if "chicks_qty" in onboard_profile_data %}' +
-                            '<input type="number" name="chicks-qty" value="{{ onboard_profile_data.chicks_qty }}" placeholder="Qty" id="chicks-qty" ' +
-                            'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                            '{% else %}' +
-                            '<input type="number" name="chicks-qty" placeholder="Qty" id="chicks_qty" ' +
-                                'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                            '{% endif %}' +
-
-                        '</div>' +
-                    '</div>' +
-                    '<div>' +
-                        '<hr class="hr-small">' +
-                    '</div>' +
-
-                    '<div class="row mb-0" id="cocks-row">' +
-                        '<div class="col-1 col-css">' +
-                            '<img src="{{ MEDIA_URL }}hen.png" alt="Cocks" class="col-css-element">' +
-                        '</div>' +
-                        '<div class="col-4 col-css">' +
-                            '<h4 class="row-name col-css-element">Cocks</h4>' +
-                        '</div>' +
-                        '<div class="col-3 col-css">' +
-                            '<p class="checkbox-label hcc-checkbox-label col-css-element">All Cocks</p>' +
-                        '</div>' +
-                        '<div class="col-1 col-css">' +
-                            '<input type="checkbox" name="all_cocks_check" id="all_cocks_check" class="col-css-element hcc-checkbox">' +
-                        '</div>' +
-                        '<div class="col-3 col-css col-css-right">' +
-                            '{% if "cocks_qty" in onboard_profile_data %}' +
-                            '<input type="number" name="cocks-qty" value="{{ onboard_profile_data.cocks_qty }}" placeholder="Qty" id="cocks-qty" ' +
-                            'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                            '{% else %}' +
-                            '<input type="number" name="cocks-qty" placeholder="Qty" id="cocks-qty" ' +
-                                'step="1" min="0" oninput="this.value = Math.abs(this.value)" class="hcc-input" onkeyup="hccTotal()">' +
-                            '{% endif %}' +
-                        '</div>' +
-                    '</div>' +
-                '</div>' +
-            '</div>';
-        document.querySelector('div[name=flocks-container]').outerHTML = flockCodeBlock;
-        }
-
-// Adds another set of feeds fields when the "Add Another" button is clicked
-    function addAnotherFeed() {
-        var feedsCodeBlock = `<div class="row">
+// FEEDS: Adds another set of fields when the "Add Another" button is clicked
+function addAnotherFeed() {
+    var feedsCodeBlock = `<div class="bottom-margin">
                             <hr class="hr-small">
                         </div>
-                        <div class="row">
+                        <div class="bottom-margin">
                             <input type="text" name="feed_name" placeholder="Feed Name" id="feed_name" required>
                         </div>
                         <div class="container">
-                            <div class="row">
+                            <div class="row bottom-margin">
                                 <div class="col-10 col-css">
                                     <input type="number" name="feed-qty-stock" placeholder="Qty in Stock" id="feed-qty-stock"
                                         step="1" min="0" oninput="this.value = Math.abs(this.value)" required>
@@ -246,63 +179,62 @@ $(document).ready(function () {
                                 </div>
                             </div>
                         </div>
-                        <div class="container" name="feeds-container"></div>`
+                        <div class="container" name="feeds-container">
+                        </div>`
         document.querySelector('div[name=feeds-container]').outerHTML = feedsCodeBlock;
         }
 
-    // Adds another set of supplements fields when the "Add Another" button is clicked
-    function addAnotherSupplement() {
-    var supplementsCodeBlock = '<div class="row">' +
-                                    '<hr class="hr-small">' +
-                                '</div>' +
-                                '<div class="row">' +
-                                    '<input type="text" name="supplement_name" placeholder="Supplement Name" id="supplement_name" required>' +
-                                '</div>' +
-                                '<div class="container">' +
-                                    '<div class="row">' +
-                                        '<div class="col-10 col-css">' +
-                                            '<input type="number" name="supplement-amount-stock" placeholder="Amount in Stock" id="supplement-amount-stock" ' +
-                                                'step="1" min="0" oninput="this.value = Math.abs(this.value)" required>' +
-                                    '</div>' +
-                                    '<div class="col-2 col-css">' +
-                                        '<h5 class="text-end">' +
-                                            'ml' +
-                                        '</h5>' +
-                                    '</div>' +
-                                '</div>' +
-                                '<div class="container" name="supplements-container"></div>';
-        document.querySelector('div[name=supplements-container]').outerHTML = supplementsCodeBlock;
+// SUPPLEMENTS: Adds another set of fields when the "Add Another" button is clicked
+function addAnotherSupplement() {
+    var supplementsCodeBlock = `<div class="bottom-margin">
+                                    <hr class="hr-small">
+                                </div>
+                                <div class="bottom-margin">
+                                    <input type="text" name="supplement_name" placeholder="Supplement Name" id="supplement_name" required>
+                                </div>
+                                <div class="container">
+                                    <div class="row bottom-margin">
+                                        <div class="col-10 col-css">
+                                            <input type="number" name="supplement-amount-stock" placeholder="Amount in Stock" id="supplement-amount-stock" 
+                                                step="1" min="0" oninput="this.value = Math.abs(this.value)" required>
+                                        </div>
+                                        <div class="col-2 col-css">
+                                            <h5 class="text-end">
+                                                ml
+                                            </h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="container" name="supplements-container">
+                                </div>`;
+    document.querySelector('div[name=supplements-container]').outerHTML = supplementsCodeBlock;
+    }
+
+// Function to display additional fields or content when a checkbpx is checked
+// Taken from http://jsfiddle.net/TrueBlueAussie/DLQY9/1/
+$(function () {
+    $('input[name="disinfectant_name"]').hide();
+
+    //show it when the checkbox is clicked
+    $('input[name="disinfected-check"]').on('click', function () {
+        if ($(this).prop('checked')) {
+            $('input[name="disinfectant_name"]').fadeIn();
+        } else {
+            $('input[name="disinfectant_name"]').hide();
         }
-
-    
-
-    // Function to make textarea height to expand based on amopunt of text added by the user
-    // Taken from https://www.techiedelight.com/automatically-resize-textarea-height-javascript/
-    $(document).ready(function() {
-        $('textarea').on('keyup keypress', function() {
-            $(this).height(0);
-            $(this).height(this.scrollHeight);
-        });
     });
+});
 
-    // Function to display additional fields or content when a checkbpx is checked
-    // Taken from http://jsfiddle.net/TrueBlueAussie/DLQY9/1/
-    $(function () {
-        $('input[name="disinfectant_name"]').hide();
-
-        //show it when the checkbox is clicked
-        $('input[name="disinfected-check"]').on('click', function () {
-            if ($(this).prop('checked')) {
-                $('input[name="disinfectant_name"]').fadeIn();
-            } else {
-                $('input[name="disinfectant_name"]').hide();
-            }
-        });
+// Function to make textarea height to expand based on amopunt of text added by the user.
+// Taken from https://www.techiedelight.com/automatically-resize-textarea-height-javascript/
+$(document).ready(function() {
+    $('textarea').on('keyup keypress', function() {
+        $(this).height(0);
+        $(this).height(this.scrollHeight);
     });
+});
 
-})
-
-// Functions to autosuggest cities. Must exist outside of (document).ready function.
+// Functions (2) to autosuggest cities.
 // Taken from https://www.youtube.com/watch?v=c3MjU9E9buQ&t=165s
 let autocomplete;
 function initAutocomplete() {
