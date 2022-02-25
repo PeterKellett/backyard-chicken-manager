9c674c152f8f5cfddd41608d1e127af3e7a12621
CathalDolan <cathal@gizagig.com>
2022-02-25 14:06:57 +0000

    Home Dashboard: Changed position of href to keep anchor within the displayed field

--- regular_tasks/models.py
+++ regular_tasks/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 class EggCollection(models.Model):
     """Model used for storing egg collection data"""
-    farm_profile = models.ForeignKey(null=False,
-                                     blank=False)
+    farm_profile = models.ForeignKey(farmprofile.id,
+                                     related_name=eggcollections)
     date = models.DateTimeField(null=False,
                                 blank=False)
-    flock = models.CharField(max_length=25,
-                             null=False,
-                             blank=False)
+    flock = models.ForeignKey(flocks.id,
+                            related_name=flocks)
     qty_total_trays = models.IntegerField(null=False,
                                           blank=False)
     qty_total_single = models.IntegerField(null=False,
@@ -34,4 +33,3 @@ class EggCollection(models.Model):
                                             blank=False)
     image_url = models.URLField(null=False,
                                 blank=False)
-    return self.type
