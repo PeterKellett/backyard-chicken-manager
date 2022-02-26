b3e5e46fcf5b17368ace7e10481ed3757554c9f3
CathalDolan <cathal@gizagig.com>
2022-02-26 19:00:12 +0000

    Home Dahsboard: Fixed positioning CSS after previous change to anchor tag locations

--- regular_tasks/models.py
+++ regular_tasks/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class EggCollection(models.Model):
     """Model used for storing egg collection data"""
-    farm_profile = models.ForeignKey(farmprofile.id,
-                                     related_name=eggcollections)
+    """farm_profile = models.ForeignKey(farmprofile.id,
+                                     related_name=eggcollections)"""
     date = models.DateTimeField(null=False,
                                 blank=False)
-    flock = models.ForeignKey(flocks.id,
-                            related_name=flocks)
+    """flock = models.ForeignKey(flocks.id,
+                            related_name=flocks)"""
     qty_total_trays = models.IntegerField(null=False,
                                           blank=False)
     qty_total_single = models.IntegerField(null=False,
@@ -22,9 +22,13 @@ class EggCollection(models.Model):
                                            blank=False)
     qty_given_free = models.IntegerField(null=False,
                                          blank=False)
-    weight_total_laid = models.DecimalField(null=False,
+    weight_total_laid = models.DecimalField(max_digits=5,
+                                            decimal_places=2,
+                                            null=False,
                                             blank=False)
-    avg_egg_weight = models.DecimalField(null=False,
+    avg_egg_weight = models.DecimalField(max_digits=5,
+                                         decimal_places=2,
+                                         null=False,
                                          blank=False)
     qty_saleable = models.IntegerField(null=False,
                                        blank=False)
