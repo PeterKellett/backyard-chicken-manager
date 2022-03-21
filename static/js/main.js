b52ed154e365548615f080aaf9fe455882ca5253
CathalDolan <cathal@gizagig.com>
2022-03-21 12:37:26 +0000

    Various changes made to forms, nothing specific. See following commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,65 +1,6 @@
 $(document).ready(function () {
     console.log("Document ready!");
   
-    // PK function
-    var flock_form_iteration = 0;
-    function addFlockForm() {
-        console.log("flock_form_iteration " + flock_form_iteration);
-        $("#form-data").append(
-            `<section name="flock-core-info">
-            <div class="bottom-margin">
-                {% if 'flock_name' in onboard_profile_data %}
-                <input type="text" name="flock_name" value="{{ onboard_profile_data.flock_name }}" placeholder="Flock Name" id="flock-name" autofocus required>
-                {% else %}
-                <input type="text" name="flock_name" placeholder="Flock Name" id="flock-name" autofocus required>
-                {% endif %}
-            </div>
-            <div class="bottom-margin">
-                {% if 'acquired_date' in onboard_profile_data %}
-                <input type="date" name="acquired_date" value="{{ onboard_profile_data.acquired_date }}" placeholder="Acquired Date" id="acquired-date" required>
-                {% else %}
-                <input type="date" name="acquired_date" placeholder="Acquired Date" id="acquired-date" required>
-                {% endif %}
-            </div>
-            <div class="bottom-margin">
-                {% if 'coop_name' in onboard_profile_data %}
-                <input type="text" name="coop_name" value="{{ onboard_profile_data.coop_name }}" placeholder="Coop Name" id="coop-name" required>
-                {% else %}
-                <input type="text" name="coop_name" placeholder="Coop Name" id="coop-name" required>
-                {% endif %}
-            </div>
-            <div class="bottom-margin">
-                <select name="breed" id="breed" required>
-                    {% if 'breed' in onboard_profile_data %}
-                    <option value="" disabled>Breed</option>
-                        {% for breed in breed %}
-                            {% if breed.id == onboard_profile_data.breed %}
-                            <option value="{{ breed.id }}" selected>{{ breed|title }}</option>
-                            {% else %}
-                            <option value="{{ breed.id }}">{{ breed|title }}</option>
-                            {% endif %}
-                        {% endfor %}
-                    {% else %}     
-                    <option value="" disabled selected>Breed</option>
-                        {% for breed in breed %}
-                        <option value="{{ breed.id }}">{{ breed|title }}</option>
-                        {% endfor %}
-                    {% endif %}
-                </select>
-                       
-                <div class="bottom-margin">
-                    <hr class="hr-small">
-                </div>
-            </div>
-        </section>`
-        )
-        flock_form_iteration++; // Increments the iteration by +1 
-    }
-    // addFlockForm();
-    $("#PKaddAnotherFlock").click(function () {
-        addFlockForm();
-    })
-
     // Show Password. Taken from https://www.w3schools.com/howto/howto_js_toggle_password.asp
     function showPasswordFunction() {
         var x = document.getElementById("password");
@@ -126,67 +67,6 @@ function injectHref() {
                 event.preventDefault();
             });
         }
-    }
-
-
-// FLOCKS: Adds another set of fields fields when the "Add Another" button is clicked
-function addAnotherFlock() {
-    var flockCodeBlock = `{% include 'html_includes/hens_chicks_cocks.html' %}`;
-    document.querySelector('div[name=flocks-container]').outerHTML = flockCodeBlock;
-    }
-
-
-// FEEDS: Adds another set of fields when the "Add Another" button is clicked
-function addAnotherFeed() {
-    var feedsCodeBlock = `<div class="bottom-margin">
-                            <hr class="hr-small">
-                        </div>
-                        <div class="bottom-margin">
-                            <input type="text" name="feed_name" placeholder="Feed Name" id="feed-name" required>
-                        </div>
-                        <div class="container">
-                            <div class="row bottom-margin">
-                                <div class="col-10 col-css">
-                                    <input type="number" name="feed-qty-stock" placeholder="Qty in Stock" id="feed-qty-stock"
-                                        step="1" min="0" oninput="this.value = Math.abs(this.value)" required>
-                                </div>
-                                <div class="col-2 col-css">
-                                    <h5 class="text-end">
-                                        kg
-                                    </h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="container" name="feeds-container">
-                        </div>`
-        document.querySelector('div[name=feeds-container]').outerHTML = feedsCodeBlock;
-        }
-
-
-// SUPPLEMENTS: Adds another set of fields when the "Add Another" button is clicked
-function addAnotherSupplement() {
-    var supplementsCodeBlock = `<div class="bottom-margin">
-                                    <hr class="hr-small">
-                                </div>
-                                <div class="bottom-margin">
-                                    <input type="text" name="supplement_name" placeholder="Supplement Name" id="supplement-name" required>
-                                </div>
-                                <div class="container">
-                                    <div class="row bottom-margin">
-                                        <div class="col-10 col-css">
-                                            <input type="number" name="supplement-amount-stock" placeholder="Amount in Stock" id="supplement-amount-stock" 
-                                                step="1" min="0" oninput="this.value = Math.abs(this.value)" required>
-                                        </div>
-                                        <div class="col-2 col-css">
-                                            <h5 class="text-end">
-                                                ml
-                                            </h5>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="container" name="supplements-container">
-                                </div>`;
-    document.querySelector('div[name=supplements-container]').outerHTML = supplementsCodeBlock;
     }
 
 
